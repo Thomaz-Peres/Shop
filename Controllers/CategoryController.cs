63f6cee6679a0677ded0b34dbcc946f31fc42fe1
Thomaz Peres <boss@thomazperes.com>
2020-07-28 22:01:56 -0300

    Initial commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
+
+[Route("categories")]
+public class CategoryController : ControllerBase
+{
+    [HttpGet]
+    [Route("")]
+    public string Get()
+    {
+        return "Get";
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public string GetById(int id)
+    {
+        return "Get" + id.ToString();
+    }
+
+    [HttpPost]
+    [Route("")]
+    public Category Post([FromBody]Category model)
+    {
+        return model;
+    }
+
+    [HttpPut]
+    [Route("{id:int}")]
+    public Category Put(int id, [FromBody]Category model)
+    {
+        if (model.Id == id)
+            return model;
+
+        return null;
+    }
+
+    [HttpDelete]
+    [Route("{id:int}")]
+    public string Delete()
+    {
+        return "Delete";
+    }
+}
